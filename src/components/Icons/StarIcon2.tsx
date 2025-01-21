daf518aa241a7bb78c58a06873bb0eb53f478ebe
Alex Bell <alexrayquaza@gmail.com>
2025-01-21 18:38:58 +1100

    WIP EVA splash screen theme

---
+++ src/components/Icons/StarIcon2.tsx
@@ -0,0 +1,58 @@
+import styles from "./StarIcon.module.scss";
+
+export default function StarIcon() {
+  return (
+    <svg
+      className={styles.starIcon}
+      width="354"
+      height="392"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <defs>
+        <filter id="svg_2_blur">
+          <feGaussianBlur in="SourceGraphic" stdDeviation="2.4" />
+        </filter>
+        <filter id="svg_3_blur" x="-50%" y="-50%" width="200%" height="200%">
+          <feGaussianBlur in="SourceGraphic" stdDeviation="3.1" />
+        </filter>
+      </defs>
+      <g>
+        <title>Layer 1</title>
+        <ellipse
+          className={styles.textColor}
+          stroke="#000"
+          cx="175.06685"
+          cy="185.13385"
+          id="svg_3"
+          rx="27.36841"
+          ry="27.36841"
+          filter="url(#svg_3_blur)"
+        />
+        <rect
+          className={styles.textColor}
+          x="162.10754"
+          y="-10.3832"
+          width="16.31995"
+          height="407.51078"
+          id="svg_1"
+          stroke="#000"
+          rx="100"
+          filter="url(#svg_2_blur)"
+          transform="rotate(33.6564 170.267 193.372)"
+        />
+        <rect
+          className={styles.textColor}
+          x="164.6083"
+          y="66.63126"
+          width="16.31995"
+          height="235.21896"
+          id="svg_2"
+          rx="100"
+          stroke="#000"
+          filter="url(#svg_2_blur)"
+          transform="rotate(-56 172.768 184.241)"
+        />
+      </g>
+    </svg>
+  );
+}
--- src/components/MainPage/SplashIntroEffect.tsx
+++ src/components/MainPage/SplashIntroEffect.tsx
@@ -1,20 +1,39 @@
 import { useEffect, useState } from "react";
 import styles from "./SplashIntroEffect.module.scss";
+import StarIcon from "../Icons/StarIcon1";
 
 export default function SplashIntroEffect() {
   const [style, setStyle] = useState({});
 
-  useEffect(() => {
-    setTimeout(function () {
-      setStyle({ display: "none" });
-    }, 2000);
-  }, []);
+  // useEffect(() => {
+  //   setTimeout(function () {
+  //     setStyle({ display: "none" });
+  //   }, 2500);
+  // }, []);
 
   return (
     <div className={styles.screen} style={style}>
-      <div className={styles.inner}>
-        <h2 className={styles.text}>Alex Bell</h2>
-        <h6 className={styles.subheader}>Software Engineer</h6>
+      <div className={styles.textWrapper}>
+        <div>
+          <h1 className={`${styles.headerText} ${styles.headerSmall}`}>
+            ALEXANDER
+          </h1>
+          <h1 className={`${styles.headerText} ${styles.headerSmall}`}>
+            HENRY
+          </h1>
+          <h1 className={`${styles.headerText} ${styles.headerLarge}`}>BELL</h1>
+          <h2 className={styles.subheader}>OCCUPATION:</h2>
+          <div className={styles.starContainerLeft}>
+            <StarIcon />
+            <StarIcon />
+            <StarIcon />
+            <StarIcon />
+            <StarIcon />
+          </div>
+        </div>
+        <div className={styles.titleWrapper}>
+          <h1 className={styles.title}>Software Engineer.</h1>
+        </div>
       </div>
     </div>
   );
